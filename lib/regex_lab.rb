82a759c66b8c536fd5756f1423a66d6638b749f6
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-22 21:17:04 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A[AEIOUaeiou]/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w^un$ing/)
 end
 
 def words_five_letters_long(text)
+  text.grap(/w{5}\s/)
 
 end
 
